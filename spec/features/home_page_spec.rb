a8326850cf73f1cdb1a3d2f2a2f761ab685c3bba
Reflekshn <reflekshn@hotmail.com>
2022-02-09 18:44:56 -0500

    Add product details capybara test

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,5 +1,3 @@
-require 'rails_helper'
-
 # RSpec.feature "HomePages", type: :feature do
 #   pending "add some scenarios (or delete) #{__FILE__}"
 # end
---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to product details from homepage", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+  
+    end
+  end
+
+  scenario "They see all products on home page" do
+    visit root_path
+
+    # commented out b/c it's for debugging only
+    # save_and_open_screenshot
+
+    expect(page).to have_css 'article.product', count: 10
+  end
+
+  scenario "They click on a product to view product details" do
+    visit root_path
+
+    # find('.products').click
+    page.find('.products').first(:link, 'Details').click
+    # commented out b/c it's for debugging only
+    # save_and_open_screenshot
+
+    expect(page).to have_css '.products-show', count: 1
+  end
+
+end
